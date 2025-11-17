c28fcba77fb45e0bfc7156b320cd959cbd8d49b0
Samuel Raj G <165649154+samuelrajgarikimukku@users.noreply.github.com>
2025-11-17 09:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.26%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,11 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits) - 1 
-        a = 0
-        for i in range(len(digits)):
-            a += digits[i] * (10**n)
-            n -=1
-        a += 1
-        result = [int(d) for d in str(a)]
-        return result
+        for i in reversed(range(len(digits))):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
 
