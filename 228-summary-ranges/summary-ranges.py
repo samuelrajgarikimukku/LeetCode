2b314bc5ebd09e00f0a81df9339acd7922343873
Samuel Raj G <165649154+samuelrajgarikimukku@users.noreply.github.com>
2026-07-13 21:16:23 +0530

    Time: 2 ms (1.78%) | Memory: 19.3 MB (23.58%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,26 +1,24 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        res = []
         if not nums:
-            return res
-        l = nums[0]
-        i = l + 1
-        r = 1
-        while r < len(nums):
-            if nums[r] != i:
-                if nums[r - 1] == l:
-                    res.append(str(l))
+            return []
+
+        res = []
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    res.append(str(start))
                 else:
-                    res.append(str(l) + "->" + str(nums[r - 1]))
-                l = nums[r]
-                i = l + 1
-                r += 1
-            else:
-                i += 1
-                r += 1
-        # Append the last pending range
-        if nums[-1] == l:
-            res.append(str(l))
+                    res.append(f"{start}->{nums[i - 1]}")
+
+                start = nums[i]
+
+        # Add the last range
+        if start == nums[-1]:
+            res.append(str(start))
         else:
-            res.append(str(l) + "->" + str(nums[-1]))
+            res.append(f"{start}->{nums[-1]}")
+
         return res
