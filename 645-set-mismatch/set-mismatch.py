8b89301d2b787717cfdbe08c9455f97588843a99
Samuel Raj G <165649154+samuelrajgarikimukku@users.noreply.github.com>
2026-08-02 23:58:19 +0530

    Time: 24 ms (26.09%) | Memory: 20.7 MB (55.28%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,10 +1,16 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        coll = {i:0 for i in range(1,len(nums)+1)}
-        for i in nums:
-            coll[i] += 1
-        dup = max(coll,key=coll.get)
-        real = min(coll,key=coll.get)
-        return [dup,real]
+        count = {i: 0 for i in range(1, len(nums) + 1)}
+
+        for num in nums:
+            count[num] += 1
+
+        for num, freq in count.items():
+            if freq == 2:
+                duplicate = num
+            elif freq == 0:
+                missing = num
+
+        return [duplicate, missing]
         
         
