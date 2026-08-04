b3f80b84e0e69f972ced065c57abccf79f7eec5e
Samuel Raj G <165649154+samuelrajgarikimukku@users.noreply.github.com>
2026-08-04 10:06:51 +0530

    Time: 18 ms (73.54%) | Memory: 20.9 MB (43.31%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.py
+++ 697-degree-of-an-array/degree-of-an-array.py
@@ -1,13 +1,37 @@
-from collections import Counter
+# from collections import Counter
+# class Solution:
+#     def findShortestSubArray(self, nums: List[int]) -> int:
+#         count = Counter(nums)
+#         degree = max(count.values())
+#         min_val = []
+#         value = [k for k, v in count.items() if v == degree]
+#         for i in value:
+#             first_appearnce = nums.index(i)
+#             last_idx = len(nums) - 1 - nums[::-1].index(i)
+#             total = last_idx - first_appearnce + 1
+#             min_val.append(total)
+#         return min(min_val)
+
+
+
+
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        count = Counter(nums)
+        first = {}
+        last = {}
+        count = {}
+
+        for i, num in enumerate(nums):
+            if num not in first:
+                first[num] = i
+            last[num] = i
+            count[num] = count.get(num, 0) + 1
+
         degree = max(count.values())
-        min_val = []
-        value = [k for k, v in count.items() if v == degree]
-        for i in value:
-            first_appearnce = nums.index(i)
-            last_idx = len(nums) - 1 - nums[::-1].index(i)
-            total = last_idx - first_appearnce + 1
-            min_val.append(total)
-        return min(min_val)
+        ans = len(nums)
+
+        for num in count:
+            if count[num] == degree:
+                ans = min(ans, last[num] - first[num] + 1)
+
+        return ans
