3cc39922f6fb2438c8f1cf310a8551e7ddc7dcc2
Samuel Raj G <165649154+samuelrajgarikimukku@users.noreply.github.com>
2026-08-27 11:26:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (6.14%) - LeetSync

--- 837-most-common-word/most-common-word.py
+++ 837-most-common-word/most-common-word.py
@@ -1,13 +1,10 @@
 from collections import Counter
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        extra = ".!@#$%^&*()_-+=[]{}\"';:.,<>?/"
 
-        for i in extra:
-            paragraph = paragraph.replace(i, " ")
-        count = Counter(paragraph.lower().split())
+        word = re.findall(r"[a-z]+",paragraph.lower())
+        count = Counter(word)
 
-        for word,frequency in count.most_common():
+        for word, freqency in count.most_common():
             if word not in banned:
                 return word
-            
