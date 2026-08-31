55163b8263dc351957a766b8a3b198c4161e817b
Samuel Raj G <165649154+samuelrajgarikimukku@users.noreply.github.com>
2026-08-31 10:35:51 +0530

    Time: 19 ms (10.54%) | Memory: 19.5 MB (1.65%) - LeetSync

--- 928-surface-area-of-3d-shapes/surface-area-of-3d-shapes.py
+++ 928-surface-area-of-3d-shapes/surface-area-of-3d-shapes.py
@@ -3,28 +3,28 @@ class Solution:
         seen = set()
         r = len(grid)
         c = len(grid[0])
-        remove = 0
         total = 0
+        remove = 0
         for i in range(r):
             for j in range(c):
-                seen.add((i,j))
                 if grid[i][j] == 0:
-                    height = 0
+                    h = 0
                 else:
-                    height = (grid[i][j] - 1) * 2
+                    h = (grid[i][j] - 1) * 2
                 v = 0
-                if i-1 >= 0 and (i-1,j) not in seen:
+                if i - 1 >= 0 and (i-1,j) not in seen:
                     min_v = min(grid[i][j], grid[i-1][j])
                     v += min_v*2
                 if i + 1 < r and (i+1,j) not in seen:
                     min_v = min(grid[i][j], grid[i+1][j])
                     v += min_v*2
-                if j-1 >= 0 and (i,j-1) not in seen:
+                if j - 1 >= 0 and (i,j-1) not in seen:
                     min_v = min(grid[i][j], grid[i][j-1])
                     v += min_v*2
-                if j+1 < c and (i,j+1) not in seen:
+                if j + 1 < c and (i,j+1) not in seen:
                     min_v = min(grid[i][j], grid[i][j+1])
                     v += min_v*2
-                remove += height + v
-                total += grid[i][j] * 6
+                seen.add((i,j))
+                total += grid[i][j]*6
+                remove += h + v
         return total - remove
